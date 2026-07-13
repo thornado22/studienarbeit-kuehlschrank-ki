38200b1ee2e88fe8a0e98a670b5053958265dbf7
Nelly <nellyt@gmx.net>
2026-07-13 23:10:19 +0200

    deleted API key

--- backend/project_config.py
+++ backend/project_config.py
@@ -1,3 +1,3 @@
-GROQ_API_KEY = "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"
+GROQ_API_KEY = "insert api key here"
 SELECTED_CNN_MODEL = "computerVision/models/test_3.json"
 PROMPT = "LLM/prompts/test_2.json"
